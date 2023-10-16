c5eee6f8307f4cfdb2753bb773e8c5d4f1e0ee95
ay7581239 <ay7581239@gmail.com>
2023-10-16 20:23:07 +0530

    intial commit

---
+++ file_of_js.js
@@ -0,0 +1,5 @@
+console.log(Document.getElementById("main_heading"));
+const main1=Document.getElementById("main_heading");
+console.log(main1.textContent);
+const link=document.querySelector(".");
+console.log()
